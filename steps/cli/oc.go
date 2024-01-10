ff37a02c0c3f3848f2150ef7e5fe3490764d1f03
Anton Misskii <amisskii@redhat.com>
2024-01-10 14:09:26 +0100

    Check if tekton resulsts secrets already exist

--- steps/cli/oc.go
+++ steps/cli/oc.go
@@ -171,7 +171,11 @@ var _ = gauge.Step("Configure the bundles resolver", func() {
 })
 
 var _ = gauge.Step("Create secrets for Tekton Results", func() {
-	oc.CreateSecretsForTektonResults()
+	if !oc.SecretExists("tekton-results-postgres", "openshift-pipelines") && !oc.SecretExists("tekton-results-tls", "openshift-pipelines"){
+		oc.CreateSecretsForTektonResults()
+	} else {
+		log.Printf("\"tekton-results-postgres\" and \"tekton-results-tls\" secrets already exist")
+	}
 })
 
 var _ = gauge.Step("Ensure that Tekton Results is ready", func() {
